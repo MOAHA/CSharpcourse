ed83b7ecebbfc35605f27bcbc40e0b24f9309289
korek pc 4 <korek pc 4@KOREKPC4-HP>
2017-09-20 10:30:38 +0300

    Add project files.

---
+++ Frist project/program muhnnd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frist_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MUHNND OMAR ABD");
+            Console.ReadKey();
+
+        }
+    }
+}
